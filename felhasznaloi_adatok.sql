f2925813e01dbefc44699dc7522354a709e88192
gicziunicorn <giczidani100@gmail.com>
2025-12-08 10:51:02 +0100

    Add files via upload

---
+++ felhasznaloi_adatok.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Dec 08. 10:49
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `felhasznalok2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznaloi_adatok`
+--
+
+CREATE TABLE `felhasznaloi_adatok` (
+  `id` int(11) NOT NULL,
+  `uname` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `jelszo` varchar(255) NOT NULL,
+  `szuldatum` date NOT NULL,
+  `telefon` varchar(11) DEFAULT NULL,
+  `nem` char(1) NOT NULL,
+  `vnev` varchar(20) DEFAULT NULL,
+  `knev` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `felhasznaloi_adatok`
+--
+
+INSERT INTO `felhasznaloi_adatok` (`id`, `uname`, `email`, `jelszo`, `szuldatum`, `telefon`, `nem`, `vnev`, `knev`) VALUES
+(6, 'alma67', 'alma@gmail.com', '$2y$10$ZKkPgAtFn4a8K1F1sITX4.tDvOL6nY6C0W/hfFzSAz52lTUNxQJ2a', '2000-10-10', '123456789', 'm', '', ''),
+(7, 'alma111', 'alma2@gmail.com', '$2y$10$I3dj4/Ti.MuEZfBZd.DpNeBhWAJ0YnW/JPumdRM2TO4o2HsU9jGqS', '2000-10-10', '123456789', 'm', '', '');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `felhasznaloi_adatok`
+--
+ALTER TABLE `felhasznaloi_adatok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `felhasznaloi_adatok`
+--
+ALTER TABLE `felhasznaloi_adatok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
